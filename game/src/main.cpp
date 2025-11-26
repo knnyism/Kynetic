328611f12ae56285d323c575dd7fe6976c0c3a0c
knnyism <icanismar@gmail.com>
2025-11-26 19:24:40 +0100

    fix: linux build (not really)

--- game/src/main.cpp
+++ game/src/main.cpp
@@ -30,7 +30,7 @@ public:
 
         m_camera = scene.add_camera(true);
 
-        m_model = scene.add_model(resources.load<kynetic::Model>("assets/shared_assets/models/sponza/sponza.gltf"));
+        m_model = scene.add_model(resources.load<kynetic::Model>("assets/shared_assets/models/sponza/Sponza.gltf"));
 
         resources.refresh_mesh_buffers();
         resources.refresh_bindless_textures();
